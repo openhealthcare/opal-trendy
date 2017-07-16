4f6308e93b3836fbc352887f8a3f47223bfdf9ae
fredkingham <fredkingham@gmail.com>
2017-07-16 12:53:15 +0100

    fixes if a link has already been selected

--- trendy/templatetags/trendy_utils.py
+++ trendy/templatetags/trendy_utils.py
@@ -17,9 +17,19 @@ def append_to_request(context, api_name, field, value):
         url, api_name, field, value
     )
 
+
 @register.filter
 def as_percentage_of(part, whole):
     try:
         return "%d%%" % (float(part) / whole * 100)
     except (ValueError, ZeroDivisionError):
         return ""
+
+
+@register.assignment_tag(takes_context=True)
+def in_request(context, subrecord_table, value, *args):
+    subrecord = subrecord_table["subrecord"]
+    field = subrecord_table["field"]
+    field_key = "{0}__{1}".format(subrecord.get_api_name(), field)
+    values = context["request"].GET.getlist(field_key, [])
+    return value in values
