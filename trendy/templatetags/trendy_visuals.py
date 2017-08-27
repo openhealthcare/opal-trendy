f482beb6f61c9631f6f51418a95b3528e9fe76e7
fredkingham <fredkingham@gmail.com>
2017-08-27 18:14:35 +0100

    with vue.js, probably not needed

--- trendy/templatetags/trendy_visuals.py
+++ trendy/templatetags/trendy_visuals.py
@@ -8,8 +8,8 @@ register = template.Library()
 
 
 @register.inclusion_tag('templatetags/pie_chart.html', takes_context=True)
-def pie_chart(context, queryset, subrecord, field):
-    subrecord = subrecords.get_subrecord_from_api_name(subrecord)
+def pie_chart(context, queryset, subrecord_api_name, field):
+    subrecord = subrecords.get_subrecord_from_api_name(subrecord_api_name)
     patients = Patient.objects.filter(episode__in=queryset)
     qs = subrecord.objects.filter(patient__in=patients)
     annotated = qs.values(field).annotate(Count('id'))
@@ -19,4 +19,7 @@ def pie_chart(context, queryset, subrecord, field):
         total_count = key_connection.pop('id__count')
         key = key_connection.values()[0]
         result.append([str(key), total_count])
-    return dict(data=json.dumps(result))
+
+    # context["graph_vals"] = json.dumps(result)
+    context["graph_vals"] = json.dumps(dict(hello='there'))
+    return context
